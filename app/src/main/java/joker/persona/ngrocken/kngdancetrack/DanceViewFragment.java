adb11ad3315ef1f9e8ac57c6e42e6f1a995d5818
Ken Ng <ng.ken84@gmail.com>
2019-01-06 18:57:04 -0800

    Implement Fragments

---
+++ app/src/main/java/joker/persona/ngrocken/kngdancetrack/DanceViewFragment.java
@@ -0,0 +1,4 @@
+package joker.persona.ngrocken.kngdancetrack;
+
+public class DanceViewFragment {
+}
--- app/src/main/java/joker/persona/ngrocken/kngdancetrack/MainActivity.java
+++ app/src/main/java/joker/persona/ngrocken/kngdancetrack/MainActivity.java
@@ -1,13 +1,30 @@
 package joker.persona.ngrocken.kngdancetrack;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private HomeFragment homeFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeFragment = new HomeFragment();
+
+        showFragment(homeFragment);
+    }
+
+    private void showFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        if(fragment.isAdded()) {
+            return;
+        }
+        transaction.replace(R.id.main_fragment_container, fragment);
+        transaction.commit();
     }
 }
